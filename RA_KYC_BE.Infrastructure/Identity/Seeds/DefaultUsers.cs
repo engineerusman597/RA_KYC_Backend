f44c21cb885f585e309f3b26dceb2e0978f35d0c
usmantppsst123 <usman@tppsst.com>
2023-10-31 22:51:07 +0500

    .

--- RA_KYC_BE.Infrastructure/Identity/Seeds/DefaultUsers.cs
+++ RA_KYC_BE.Infrastructure/Identity/Seeds/DefaultUsers.cs
@@ -54,6 +54,54 @@ namespace Infrastructure.Identity.Seeds
             }
             #endregion
 
+            #region Default Admin 1
+
+            var defaultAdmin1 = new AppUser
+            {
+                UserName = "admin@kyc.com",
+                Email = "admin@kyc.com",
+                FirstName = "Admin",
+                LastName = "User",
+                EmailConfirmed = true,
+                IsApproved = true,
+                PhoneNumberConfirmed = true
+            };
+
+            if (userManager.Users.All(u => u.Id != defaultAdmin1.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultAdmin1.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultAdmin1, "Admin@1234");
+                    await userManager.AddToRoleAsync(defaultAdmin1, Roles.Admin.ToString());
+                }
+            }
+            #endregion
+
+            #region Default Admin 2
+
+            var defaultAdmin2 = new AppUser
+            {
+                UserName = "whitney.rolle@tppsst.com",
+                Email = "whitney.rolle@tppsst.com",
+                FirstName = "Whitney",
+                LastName = "Rolle",
+                EmailConfirmed = true,
+                IsApproved = true,
+                PhoneNumberConfirmed = true
+            };
+
+            if (userManager.Users.All(u => u.Id != defaultAdmin2.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultAdmin2.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultAdmin2, "Admin@1234");
+                    await userManager.AddToRoleAsync(defaultAdmin2, Roles.Admin.ToString());
+                }
+            }
+            #endregion
+
         }
     }
 }
---
+++ RA_KYC_BE.Infrastructure/Migrations/20231031173315_Added IsApproved.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RA_KYC_BE.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIsApproved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "Identity.User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "Identity.User");
+        }
+    }
+}
